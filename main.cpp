d2b8e0d81bf49a8e7a39a7043629e2d59b6b9ea5
ethan-steider <150487933+ethan-steider@users.noreply.github.com>
2025-01-13 13:29:52 -0500

    test code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    
+    int x = 23;
+    int y = 54;
+
+    int sum = x + y;
+
+    std::cout << y << '\n';
+    std::cout << x << '\n';
+    std::cout << sum << '\n';
+
+    return 0;
+} 
