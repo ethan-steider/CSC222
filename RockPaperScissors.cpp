18de7a931559d815c0b2722720ccce10bd9fad6f
ethan-steider <150487933+ethan-steider@users.noreply.github.com>
2025-01-15 13:13:06 -0500

    started writing out requirements

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -1,1 +1,12 @@
 
+#include <iostream>
+using namespace std;
+
+// Function prototype 
+
+
+// Program should loop until user decides to quit
+// Random number 1-3 1 is rock 2 is paper 3 is scissors
+// Human enters "Rock", "Paper", "Scissors" or "Quit"
+// Program compares human input to random number and determines winner
+
