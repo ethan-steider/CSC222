68b91c9836f9807c142e6f87879ece0bbf30467f
ethan-steider <150487933+ethan-steider@users.noreply.github.com>
2025-01-15 16:16:05 -0500

    started the display function and playGame function
    once playtime is working with preset numbers will work on using the random number package

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -1,12 +1,26 @@
-
 #include <iostream>
 using namespace std;
-
-// Function prototype 
-
 
 // Program should loop until user decides to quit
 // Random number 1-3 1 is rock 2 is paper 3 is scissors
 // Human enters "Rock", "Paper", "Scissors" or "Quit"
 // Program compares human input to random number and determines winner
+// if both are the same, it's a draw
+
+
+// Function prototypes
+
+void displayChoice();
+void playGame(char choice);
+
+
+void displayChoice()
+{
+    char choice; // take user input
+    cout << "Enter Rock, Paper, Scissors, or Quit: "; // prompts user to enter choice
+    cin >> choice; // takes in chocie
+    cout << "You chose: " << choice << endl; //tells user what they chose
+    if choice == 'Quit'
+    playGame(choice); //plays game with choice
+}
 
