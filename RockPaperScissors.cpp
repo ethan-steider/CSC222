e1467082e53dea1f572ba93635f3c3781ddbd38a
ethan-steider <150487933+ethan-steider@users.noreply.github.com>
2025-01-15 20:08:22 -0500

    added scissors choice and function prototypes for scorekeeping

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -2,19 +2,34 @@
 #include <random>
 using namespace std;
 
+// Requirements for the game
+// ***********************************************
 // Program should loop until user decides to quit
 // Random number 1-3 1 is rock 2 is paper 3 is scissors
 // Human enters "Rock", "Paper", "Scissors" or "Quit"
 // Program compares human input to random number and determines winner
 // if both are the same, it's a draw
+//`***********************************************
 
+// Random number engine
+random_device engine;
+ 
+// Distribution object
+uniform_int_distribution<int> dist(1, 3);
 
 // Function prototypes
 
 void displayChoice();
 void playGame(char choice);
+void keepScore(char winner);
+void displayScore(char winner);
 
 
+
+void playGame(char choice) {
+
+
+}
 
 void displayChoice()
 {
@@ -30,6 +45,9 @@ void displayChoice()
     }
     else if (choice == 'Paper') {
         playGame(choice); //plays game with paper
+    }
+    else if (choice == 'Scissors') {
+        playGame(choice); // plays game with scissors
     }
 }
 
