465e4cdfe176ff0e42c61f1639a4a73a3e574690
Ethan <ethan.steider.22@cnu.edu>
2025-02-17 12:52:48 -0500

    created prototypes

---
+++ ReverseArray.cpp
@@ -0,0 +1,35 @@
+// program populated array with values
+//function then called to reverse the array
+// memory is deallocated in the function before it terminates
+// function one reverses array by dynamicly creating array of size of input parameter
+// then it copies the values from the input array to the new array in reverse order
+// receives a constant array and its size returns pointer to the new array
+// function two receives a constant array and its size then displays the contents of the array
+
+#include <iostream>
+using namespace std;
+// function prototypes
+
+int *ReverseArray( int const arr[], int size );
+void PrintArray(int const arr[], int size);
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+    return 0;
+}
