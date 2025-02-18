cfe83a648ad0fce674b427d2ab95e4dc79a8a3df
Ethan <ethan.steider.22@cnu.edu>
2025-02-18 15:42:32 -0500

    started gradebook 2 electric boogaloo

---
+++ CourseGrade.cpp
@@ -0,0 +1,4 @@
+// same output as GradeBook but with added student ID
+// a function that reads data from a file containing number of students, number of tests as header 
+//followed by records containing name, student ID, and test scores. Function assumes file is already open 
+// receives file variable as input
