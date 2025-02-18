fc0a63354778dd492150da13207f097239b5ac6e
Ethan <ethan.steider.22@cnu.edu>
2025-02-18 16:05:35 -0500

    started getdata using GradeBook function as starting point

--- CourseGrade.cpp
+++ CourseGrade.cpp
@@ -4,20 +4,32 @@
 #include <string>
 using namespace std;
 
+const int Max_cols = 5;
+const int Max_Rows = 15; // global constants
+const string FileName = "StudentGrades.txt";
+
 // function prototypes
 int getData(istream &inFile, string names[], int data[][Max_cols]); //function to read in data from file
 char getLetterGrade(double average);   //function to calculate letter grade
 
 
-
 // a function that reads data from a file containing number of students, number of tests as header 
-//followed by records containing name, student ID, and test scores. Function assumes file is already open 
+// followed by records containing name, student ID, and test scores. Function assumes file is already open 
 // receives file variable as input
+int getData(istream &inFile, string names[], int data[][Max_cols]) {
+    int records = 0;
+    while(inFile >> names[records]) { // read name
+        inFile >> data[records][0]; // read student ID
+        for(int i = 1; i <= Max_cols; i++) { // read test scores
+            inFile >> data[records][i];
+        }
+        records++;
+    }
+    return records;
+}
 
 
-
-
-// a function that reads the number of student records and the number of tests from the file. It
+// A function that reads the number of student records and the number of tests from the file. It
 // dynamically allocates an array of structs to hold the student records. It uses a loop to read
 // and store the student’s name and ID in the struct for each record. Before it reads the test
 // scores for the record, it dynamically allocates an array to hold the test scores. When the
@@ -27,7 +39,7 @@ char getLetterGrade(double average);   //function to calculate letter grade
 
 
 
-//A function that is provided with the student data array and uses the test scores that are
+// A function that is provided with the student data array and uses the test scores that are
 // stored there to calculate the average of the test grades and the course grade for each
 // student. These values are stored in the struct. Use the function you wrote in the original
 // version to calculate the letter grade. Here is the prototype for this function
@@ -38,7 +50,7 @@ char getLetterGrade(double average);   //function to calculate letter grade
 
 
 
-//A function that is provided with the student data array and prints a formatted report.
+// A function that is provided with the student data array and prints a formatted report.
 
 
 
