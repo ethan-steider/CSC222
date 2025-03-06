04707baf60599ed89e4de39b384cc025302b3e4c
Ethan <ethan.steider.22@cnu.edu>
2025-03-05 20:30:52 -0500

    continued class

--- Project3/DateClass.cpp
+++ Project3/DateClass.cpp
@@ -32,3 +32,24 @@ void printDate1();
 void printDate2();
 void printDate3();
 
+Date(int = 1, int = 1, int = 1900);
+
+class Date {
+    private:
+        int month;
+        int day;
+        int year;
+    public:
+        void setDate(int m, int day, int y);
+        int getMonth();
+        int getDay();
+        int getYear();
+        bool isLeapYear();
+        bool isLeapYear(int year);
+        int lastDay();
+        int lastDay(int month, int year);
+        void printDate1();
+        void printDate2();
+        void printDate3();
+        Date(int m = 1, int d = 1, int y = 1900);
+};
