95f5171a866f73a578311bc82e9b285ae22fa8ec
Ethan <ethan.steider.22@cnu.edu>
2025-03-05 20:20:50 -0500

    started class with prorptypes

--- Project3/DateClass.cpp
+++ Project3/DateClass.cpp
@@ -6,9 +6,29 @@
 // do not accept values for day greater than last day of month or less than
 // do not accept values for month greater than 12 or less than 1
 
+// date is represented via three integers the month is an integer between 1 and 12, day is an integer 
+//between 1 and the last day of that month the year is a positive integer. The default date is 1/1/1900
+
 
 #include <iostream>
 #include <string>
 #include <iomanip>
 using namespace std;
+
+
+void setDate(int m, int day, int y);
+
+int getMonth();
+int getDay();
+int getYear();
+
+bool isLeapYear();
+bool isLeapYear(int year);
+
+int lastDay();
+int lastDay(int month, int year);
+
+void printDate1();
+void printDate2();
+void printDate3();
 
