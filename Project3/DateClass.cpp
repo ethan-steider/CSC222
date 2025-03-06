fb5a3a7fd3daef2119837d1a91bcd04d6c28be45
Ethan <ethan.steider.22@cnu.edu>
2025-03-05 20:15:31 -0500

    started dateclass

---
+++ Project3/DateClass.cpp
@@ -0,0 +1,14 @@
+// Date class should store a date in three integers month day and year. member functions to print
+// the date in the following formats:
+// 12/25/2021
+// December 25, 2021
+// 25 December 2021
+// do not accept values for day greater than last day of month or less than
+// do not accept values for month greater than 12 or less than 1
+
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
